111c9e6883b5444acb2d803b614d334c43a0380e
ihebbgd <ihebbgd0@gmail.com>
2026-01-29 04:57:53 +0100

    http handling

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -21,8 +21,13 @@ public class UserController {
 
 
 
-    @GetMapping("/users/{id}")
-    public User getUserById(@PathVariable Long id){
-        return userRepository.findById(id).orElse(null);
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        var user = userRepository.findById(id).orElse(null);
+        if(user==null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 }
