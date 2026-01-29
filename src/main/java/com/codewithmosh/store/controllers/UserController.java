868a03ec30cf3edc29d85a8b8e61e8e971ec1c90
ihebbgd <ihebbgd0@gmail.com>
2026-01-29 04:47:05 +0100

    Add controllers and entities for user and message management

---
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -0,0 +1,28 @@
+package com.codewithmosh.store.controllers;
+
+
+import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class UserController {
+    private final UserRepository userRepository;
+
+
+    @GetMapping("/users")
+    public Iterable<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable Long id){
+        return userRepository.findById(id).orElse(null);
+    }
+}
